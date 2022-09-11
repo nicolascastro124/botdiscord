521b51fd528363326a3040155614c5fdf55dd9da
Nicolas Castro <nicolascastro124@gmail.com>
2022-09-11 02:58:17 -0300

    v1.0.4

--- index.js
+++ index.js
@@ -9,28 +9,7 @@ const path = require('path');
 
 const dirEvents = fs.readdirSync(path.join(__dirname, 'events'));
 
-const hour = require("./events/hour")
 
-/////////////
-/*
-const keepAlive = require('./server');
-const Monitor = require('ping-monitor');
-
-keepAlive();
-const monitor = new Monitor({
-  website: 'LINK',
-  title: 'Nombre',
-  interval: 5 // minutes
-});
-
-monitor.on('up', (res) => console.log(`${res.website} está encedido.`));
-monitor.on('down', (res) => console.log(`${res.website} se ha caído - ${res.statusMessage}`));
-monitor.on('stop', (website) => console.log(`${website} se ha parado.`));
-monitor.on('error', (error) => console.log(error));
-*/
-
-
-////////
 for (const fileEvent of dirEvents) {
   const event = require(path.join(__dirname, 'events', fileEvent));
   client.on(event.name, (...args) => event.run(client, ...args));
@@ -51,13 +30,14 @@ setInterval(function() {
   var hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
   var dia = hoy.getDay()
 
-  if (hora == '1:0:0' && dia!= '0') {
+  if (hora == '1:0:0') {
     client.channels.resolve(process.env['GENERAL']).send("¡El día se ha reiniciado, es hora de quemar el FP! 🔥 ⏰");
-    if(dia == '0'){
-      client.channels.resolve(process.env['GENERAL']).send("Una nueva semana ha comenzado, el limite de recompensa de algunas raids se ha restablecido.:alarm_clock::alarm_clock::alarm_clock::warning:");
-    }
+  
   }
-  client.channels.resolve(process.env['GENERAL']).send(hora);
+  if(dia == '1'){
+    client.channels.resolve(process.env['GENERAL']).send("Una nueva semana ha comenzado, el limite de recompensa de algunas raids se ha restablecido.:alarm_clock::alarm_clock::alarm_clock::warning:");
+  } 
+  
 
 }, 1000 )
 
